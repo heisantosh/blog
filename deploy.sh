3b43171f9d5185008b41553e6a220ce3c44528d0
Santosh Heigrujam <santosh.hei@gmail.com>
2020-03-04 00:02:52 +0530

    Update theme

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t mu -d heisantosh.github.io
+hugo -t noteworthy -d heisantosh.github.io
 
 # Go To Public folder
 cd heisantosh.github.io
